008edab31efd6d77ff76d798241327441e188fd5
Josef Reichelt <pepareichelt@gmail.com>
2023-12-06 18:20:45 +0100

    Day 4 part 2

--- day04/part2/solution.py
+++ day04/part2/solution.py
@@ -14,22 +14,22 @@ class Card:
 
 def process_card_list(cards: list[Card]):
     amount = 0
+    card_multiplier: dict[str, int] = {}
     for card in cards:
-        print(f"Processing: {card.card_id}")
-        next_cards_to_copy = 0
-        for number in card.numbers:
-            for winning_number in card.winning_numbers:
-                if number == winning_number:
-                    next_cards_to_copy += 1
-        print(f"Coppying next {next_cards_to_copy} cards")
-        cards_to_process = filter(lambda c: int(c.card_id) in range(
-            int(card.card_id) + 1, int(card.card_id) + next_cards_to_copy + 1), cards.copy())
-        print("Cards to process")
-        print(list(cards_to_process))
-        
-        # for coppied_cards in range(next_cards_to_copy):
-        amount += process_card_list(list(cards_to_process))
-
+        current_multiplier = card_multiplier.get(card.card_id, 1)
+        # print(f"Multiplier {current_multiplier} - {range(current_multiplier)}")
+        for _ in range(current_multiplier):
+            # print(f"Processing: {card.card_id}")
+            next_cards_to_copy = int(card.card_id)
+            amount += 1
+            for number in card.numbers:
+                for winning_number in card.winning_numbers:
+                    if number == winning_number:
+                        next_cards_to_copy += 1
+                        # print(f"Winning card")
+                        if str(next_cards_to_copy) not in card_multiplier:
+                            card_multiplier[str(next_cards_to_copy)] = 1
+                        card_multiplier[str(next_cards_to_copy)] += 1
     return amount
 
 
@@ -38,7 +38,6 @@ def solve(input: list[str]):
     cards: list[Card] = []
     for line in input:
         card_id = line[line.index(" ") + 1:line.index(":")].strip()
-        print(card_id)
         card_info = line[line.index(":") + 1:].split("|")
         numbers = list(map(lambda num: int(num), card_info[0].split()))
         winning_numbers = list(map(lambda num: int(num), card_info[1].split()))
